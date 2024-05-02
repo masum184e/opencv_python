87dd1947bb5fadfa2e9869c791d53a5048dee2ec
masum184e <masum184e@gmail.com>
2024-05-02 15:08:50 +0600

    line & circle drawing added

---
+++ 4.drawCircle.py
@@ -0,0 +1,15 @@
+import cv2 as openCV
+import numpy as np
+
+image = 255 * np.ones((512, 512, 3), dtype=np.uint8)
+
+center_coordinates = (256, 256)
+radius = 100
+color = (255, 0, 0)
+thickness = 2
+
+openCV.circle(image, center_coordinates, radius, color, thickness)
+
+openCV.imshow('Line Image', image)
+openCV.waitKey(0)
+openCV.destroyAllWindows()
