fadf5e771b44a357af590e5d5aa5c74ab4612f12
masum184e <masum184e@gmail.com>
2024-05-02 15:01:43 +0600

    image capturing added

---
+++ 2.captureImage.py
@@ -0,0 +1,20 @@
+import cv2 as openCV
+    
+camera = openCV.VideoCapture(0)
+
+if not camera.isOpened():
+    print("Error: Failed to open camera.")
+
+ret, frame = camera.read()
+
+if not ret:
+    print("Error: Failed to capture frame.")
+
+openCV.imshow('Captured Image', frame)
+
+image_path = 'images/captured_image.jpg'
+openCV.imwrite(image_path, frame)
+print(f"Image saved as {image_path}")
+
+camera.release()
+openCV.destroyAllWindows()
