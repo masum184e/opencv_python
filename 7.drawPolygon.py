03d802468fa29a583745b20f0e852cf282b63821
masum184e <masum184e@gmail.com>
2024-05-02 23:06:11 +0600

    polygon added

---
+++ 7.drawPolygon.py
@@ -0,0 +1,23 @@
+import cv2 as openCV
+import numpy as np
+
+image = 255 * np.ones((512, 512, 3), dtype=np.uint8)
+
+pts = np.array([
+        [100, 100], 
+        [300, 100], 
+        [400, 200], 
+        [200, 400]
+    ],np.int32
+)
+
+pts = pts.reshape((-1, 1, 2))
+isClosed = True
+color = (255, 0, 0)
+thickness = 2
+
+openCV.polylines(image, [pts], isClosed, color, thickness)
+
+openCV.imshow('Line Image', image)
+openCV.waitKey(0)
+openCV.destroyAllWindows()
