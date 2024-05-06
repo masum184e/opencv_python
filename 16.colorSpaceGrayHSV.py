d51aa28eead20ef6b17b8bd327139a02d0dd79a7
masum184e <masum184e@gmail.com>
2024-05-06 09:49:29 +0600

    blurred_image added

--- 16.colorSpaceGrayHSV.py
+++ 16.colorSpaceGrayHSV.py
@@ -12,9 +12,11 @@ upper_blue = np.array([130, 255, 255])
 
 mask = cv.inRange(hsv_img, lower_blue, upper_blue)
 blue_object = cv.bitwise_and(img, img, mask=mask)
+blurred_image = cv.GaussianBlur(img, (3,3), cv.BORDER_DEFAULT)
 
 cv.imshow('Original Image', img)
 cv.imshow('Grayscale Image', gray_img)
+cv.imshow('Blur Image', blurred_image)
 cv.imshow('HSV Image', hsv_img)
 cv.imshow('Extracted Blue Object', blue_object)
 
