c58a5db433eb6cccccf343dfa303a2b4b8232e24
masum184e <masum184e@gmail.com>
2024-06-15 10:34:21 +0600

    example added

--- 13.regionOfInterest.py
+++ 13.regionOfInterest.py
@@ -9,3 +9,7 @@ roi = image[y:y+h, x:x+w]
 openCV.imshow('ROI', roi)
 openCV.waitKey(0)
 openCV.destroyAllWindows()
+
+# For eye detection in images, first face detection is done over the entire image.
+# When a face is obtained, we select the face region alone and search for eyes inside it instead of searching the whole image.
+# It improves accuracy (because eyes are always on faces :D ) and performance (because we search in a small area).
